2b4c0bff120477af8fddd3092ef94136f04f8cdf
jennifernghinguyen <jennifernghi@yahoo.com>
2017-02-19 03:48:33 -0800

    DetectMultipleOfTen

--- CS 49C/lab3/DetectMultipleOfTen.c
+++ CS 49C/lab3/DetectMultipleOfTen.c
@@ -9,11 +9,18 @@
 #include <stdio.h>
 int main(void){
     int i;
+    
     for(i =1; i<=100; i++){
-        if(i%10==0){
+        
+        if(i%10==0){ // at 10th asterisk, print new line
+            
             printf(" * \n");
+            
         }else{
+            
             printf(" * ");
+            
         }
+        
     }
 }
